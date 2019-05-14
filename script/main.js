06fe8e9ecc7e4805475de085e04e4fd0712fce21
randobigor <bodnar.igor.98@gmail.com>
2019-05-14 08:47:13 +0300

    First Commit

---
+++ script/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+    document.querySelector('#myButton').addEventListener('click', function () {
+        alert('It Works');
+    });
+});
---
+++ script/main.ts
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    document.querySelector('#myButton').addEventListener('click', () => {
+        alert('It Works')
+    })
+})
